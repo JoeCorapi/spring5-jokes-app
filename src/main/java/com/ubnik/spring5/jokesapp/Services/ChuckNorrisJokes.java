b2754a6105b5e31bfb9e68fcd012721cfd8a8106
Joe Corapi <24542019+JoeCorapi@users.noreply.github.com>
2018-02-18 21:13:23 -0500

    Create chuck norris implementation of joke service

---
+++ src/main/java/com/ubnik/spring5/jokesapp/Services/ChuckNorrisJokes.java
@@ -0,0 +1,19 @@
+package com.ubnik.spring5.jokesapp.Services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChuckNorrisJokes implements JokesService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public ChuckNorrisJokes() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
