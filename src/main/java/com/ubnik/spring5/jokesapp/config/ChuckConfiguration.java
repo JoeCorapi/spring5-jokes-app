f76f339fbe246dbe33b32a97d943defe7b097cba
Joe Corapi <24542019+JoeCorapi@users.noreply.github.com>
2018-02-19 23:24:41 -0500

    Refactor to practice XML bean configuration

--- src/main/java/com/ubnik/spring5/jokesapp/config/ChuckConfiguration.java
+++ src/main/java/com/ubnik/spring5/jokesapp/config/ChuckConfiguration.java
@@ -7,10 +7,10 @@ import org.springframework.context.annotation.Configuration;
 /**
  * Created by Joe Corapi on 2/19/2018.
  */
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
-    @Bean
+    //@Bean
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
